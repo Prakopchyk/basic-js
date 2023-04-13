8d3b5850ccba30f77bf60cafae54bbc827d5e823
Prakopchyk <prokopchike1987@gmail.com>
2023-04-13 18:13:52 +0200

    feat: implement task matrix-elements-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+
+  function transpose(m) {
+    return m[0].map((col, i) => m.map(row => row[i]));
+  }
+
+  let transposedMatrix = transpose(matrix);
+  let sum = 0;
+  internalLoop: for (let i = 0; i < transposedMatrix.length; i++) {
+    let arr = transposedMatrix[i];
+    for (let j = 0; j < arr.length; j++) {
+      let num = arr[j];
+      if (num === 0) {
+        continue internalLoop;
+      }
+      sum += num;
+    }
+
+  }
+
+  return sum;
 }
 
 module.exports = {
